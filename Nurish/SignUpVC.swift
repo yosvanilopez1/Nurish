6817f2db9d0b77ab746d986e9c4ed672471fde0b
yosvanilopez1 <yosvanilopez@Yosvanis-MacBook-Pro.local>
2017-06-10 15:22:25 -0400

    Everything

--- Nurish/SignUpVC.swift
+++ Nurish/SignUpVC.swift
@@ -9,27 +9,18 @@
 import UIKit
 
 class SignUpVC: UIViewController {
-
+    @IBOutlet weak var nameInputField: UITextField!
+    @IBOutlet weak var emailInputField: UITextField!
+    @IBOutlet weak var addressInputField: UITextField!
+    @IBOutlet weak var phoneInputField: UITextField!
+    @IBOutlet weak var passwordInputField: UITextField!
+    @IBOutlet weak var codeInputField: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ Nurish/UnderlinedTextField.swift
@@ -0,0 +1,23 @@
+//
+//  UnderlinedTextField.swift
+//  Nurish
+//
+//  Created by Yosvani Lopez on 6/10/17.
+//  Copyright © 2017 Yosvani Lopez. All rights reserved.
+//
+
+import UIKit
+
+class UnderlinedTextField: UITextField {
+
+    override func awakeFromNib() {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.lightGray.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+
+}

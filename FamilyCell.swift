dac7a31cd86c6b7145016b4408a54592a00433ec
yosvanilopez1 <yosvanilopez@Yosvanis-MacBook-Pro.local>
2017-06-10 16:37:00 -0400

    pods and chat

---
+++ FamilyCell.swift
@@ -0,0 +1,24 @@
+//
+//  FamilyCell.swift
+//  Nurish
+//
+//  Created by Yosvani Lopez on 6/10/17.
+//  Copyright © 2017 Yosvani Lopez. All rights reserved.
+//
+
+import UIKit
+
+class FamilyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Nurish/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  Nurish
+//
+//  Created by Yosvani Lopez on 6/10/17.
+//  Copyright © 2017 Yosvani Lopez. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+    
+    
+}
